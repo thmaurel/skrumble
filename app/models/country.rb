06379cd33cd459964393c89d3041b9ba4cd091f0
Thibaud Maurel <maurel.thibaud@free.fr>
2019-06-04 15:42:31 +0200

    photos liées aux pays

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,6 @@
 class Country < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
+
   has_many :trips
   has_many :tasks
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,10 +18,23 @@ u.save
 puts "User created"
 
 ct = ["France", "Germany", "Australia", "USA", "New Zealand", "Brasil", "Canada", "South Africa", "Japan", "Mauricius"]
+pic = ["https://study-eu.s3.amazonaws.com/uploads/image/path/97/wide_fullhd_france-paris-eiffel-tower.jpg",
+  "https://www.weekend-evasion.fr/wp-content/uploads/2014/11/Cath%C3%A9drale-de-Cologne.jpg",
+  "https://www.voyageavecnous.fr/wp-content/uploads/2018/10/quel-visa-pour-australie.jpg",
+  "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.travelandleisure.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F1600x1000%2Fpublic%2F1551194229%2Fgrand-canyon-south-rim-19-GRANDCANYON0219.jpg%3Fitok%3DpXzOkx5_&q=85",
+  "https://destination-nouvellezelande.com/wp-content/uploads/2018/02/incontournable-nouvelle-zelande.jpg",
+  "https://www.tribudexplorateurs.com/wp-content/uploads/2018/11/voyage-bresil-famille-rio-1.jpg",
+  "https://www.voyagetips.com/wp-content/uploads/2018/01/yukon-canada.jpg",
+  "https://lonelyplanetimages.imgix.net/mastheads/stock-photo-welcome-to-cape-town-104168527.jpg?sharp=10&vib=20&w=1200",
+  "https://www.readytogo.fr/files/fichepays/Japon_1469092036_shutterstock_242376076-min.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2KPXDyYwXWvJ2WkloJ566PenrKdmpfTR3RziyAKj5Whgyx36JHw"
+]
 
-ct.each do |name|
+
+ct.each_with_index do |name, i|
   c = Country.new
   c.name = name
+  c.remote_photo_url = pic[i]
   c.save
 end
 
