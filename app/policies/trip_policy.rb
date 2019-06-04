b227fbde4443ba6310ca8bf466a28d4e775976b9
Thibaud Maurel <maurel.thibaud@free.fr>
2019-06-04 11:44:35 +0200

    pundit

---
+++ app/policies/trip_policy.rb
@@ -0,0 +1,15 @@
+class TripPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    record.user == user
+  end
+
+  def create?
+    return true
+  end
+end
