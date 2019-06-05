259f3b5ecf45b47d51f3c971459f7cfc1410aa2f
Victor Authiat <authiatv@gmail.com>
2019-06-05 11:50:47 +0200

    flatpicker

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,10 +1,12 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 
+
 const initFlatpickr = () => {
   const datepicker = document.querySelector(".datepicker")
   if (datepicker) {
-    flatpickr(".datepicker", {})
+    flatpickr(".datepicker", {
+    })
   }
 }
 
