af04ab02c4cf794ddca771272c7117973f2ddba6
Victor Authiat <authiatv@gmail.com>
2019-06-10 15:02:05 +0200

    ajout de spinner loading

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,9 @@ import "bootstrap";
 import "../plugins/flatpickr";
 import { initSortable } from '../plugins/init_sortable';
 import initFlatpickr from "../plugins/flatpickr";
+import initSpin from "../plugins/spin";
 
 
 initSortable();
 initFlatpickr();
+initSpin();
