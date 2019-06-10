c601d25d7d9206e4965eacc2e28d17a499593910
Thibaud Maurel <maurel.thibaud@free.fr>
2019-06-10 18:55:37 +0200

    correctif

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,14 +2,15 @@ import "bootstrap";
 import "../plugins/flatpickr";
 import { initSortable } from '../plugins/init_sortable';
 import initFlatpickr from "../plugins/flatpickr";
-import { initSelect2 } from '../plugins/init_select2';
+import initSelect2 from '../plugins/init_select2';
 import initCollapsible from "../plugins/init_collapsible";
 import swal from 'sweetalert';
 import initSpin from "../plugins/spin";
-import initSelect2 from '../plugins/init_select2';
+
 
 initSortable();
 initFlatpickr();
-initSpin();
 initSelect2();
+initSpin();
+
 initCollapsible();
