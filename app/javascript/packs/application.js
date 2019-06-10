d6ab0ad574c1eea1e7f095ef9c363a3268f3a07c
Thibaud Maurel <maurel.thibaud@free.fr>
2019-06-10 11:30:13 +0200

    select2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,10 @@ import "bootstrap";
 import "../plugins/flatpickr";
 import { initSortable } from '../plugins/init_sortable';
 import initFlatpickr from "../plugins/flatpickr";
+import { initSelect2 } from '../plugins/init_select2';
+
 
 
 initSortable();
 initFlatpickr();
+initSelect2();
