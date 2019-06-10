0bd20f109198d1ec21af4d878fa4aadbb9889060
Raphael Mantello <mantello.raphael@gmail.com>
2019-06-10 17:23:41 +0200

    Share button works

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import "bootstrap";
 import "../plugins/flatpickr";
 import { initSortable } from '../plugins/init_sortable';
 import initFlatpickr from "../plugins/flatpickr";
+import swal from 'sweetalert';
 
 
 initSortable();
